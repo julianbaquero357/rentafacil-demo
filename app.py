cb94815f69bee62e6bfb3bf1280c927803ef6d66
julianbaquero357 <est.julian.baquero@unimilitar.edu.co>
2026-02-12 20:05:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -48,5 +49,7 @@ def consultar():
 
     return render_template("resultado.html", data=resultado)
 
+# IMPORTANTE: configuración para Render
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
